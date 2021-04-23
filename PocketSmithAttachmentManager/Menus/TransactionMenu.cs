e2253ce518558e5fc8ba019557a53fd471392256
Nate Burdick <nate@thep4x.com>
2021-04-23 07:10:40 -0500

    Finished Transaction Upload Functionality

--- PocketSmithAttachmentManager/Menus/TransactionMenu.cs
+++ PocketSmithAttachmentManager/Menus/TransactionMenu.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using PocketSmithAttachmentManager.Models;
 using PocketSmithAttachmentManager.Services;
+using PocketSmithAttachmentManager.Services.Extensions;
 
 namespace PocketSmithAttachmentManager.Menus
 {
@@ -60,6 +64,145 @@ namespace PocketSmithAttachmentManager.Menus
                     break;
                 }
             }
+
+
+        }
+
+        public static async Task<TransactionModel> GetTransactionByAmount()
+        {
+            decimal transactionAmount = 0.00m;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter the transaction amount or [C]ancel and press ENTER:");
+
+                try
+                {
+                    var selectedOption = Console.ReadLine();
+                    if (selectedOption.ToLower() == "c")
+                    {
+                        await LocalAttachmentMenu.Show();
+                    }
+
+                    transactionAmount = decimal.Parse(selectedOption);
+                }
+                catch (Exception)
+                {
+                    transactionAmount = 0.00m;
+                }
+            } while (transactionAmount <= 0.00m);
+
+            var transactions = await _transactionService.GetTransactionsByAmount(transactionAmount);
+
+            return await selectTransaction(transactions);
+        }
+
+        public static async Task<TransactionModel> GetTransactionByDate()
+        {
+            DateTime? startDate = null;
+            DateTime? endDate = null;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter a start date or [C]ancel and press ENTER:");
+
+                try
+                {
+                    var selectedOption = Console.ReadLine();
+                    if (selectedOption.ToLower() == "c")
+                    {
+                        await LocalAttachmentMenu.Show();
+                    }
+
+                    startDate = DateTime.Parse(selectedOption);
+                }
+                catch (Exception)
+                {
+                    startDate = null;
+                }
+            } while (startDate == null);
+
+            do
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter an end date or [C]ancel and press ENTER:");
+
+
+                    var selectedOption = Console.ReadLine();
+                    if (selectedOption.ToLower() == "c")
+                    {
+                        await LocalAttachmentMenu.Show();
+                    }
+
+                    endDate = string.IsNullOrEmpty(selectedOption) ? startDate : DateTime.Parse(selectedOption);
+                }
+                catch (Exception)
+                {
+                    endDate = null;
+                }
+
+            } while (endDate == null);
+
+            var transactions = await _transactionService.GetTransactionsByDate(Convert.ToDateTime(startDate), endDate);
+
+            return await selectTransaction(transactions);
+
+        }
+
+        public static async Task<TransactionModel> GetTransactionByPayee()
+        {
+            string transactionPayee = null;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter the payee name or [C]ancel and press ENTER:");
+                transactionPayee = Console.ReadLine();
+
+                if (transactionPayee.ToLower() == "c")
+                {
+                    await LocalAttachmentMenu.Show();
+                }
+            } while (string.IsNullOrEmpty(transactionPayee));
+
+            var transactions = await _transactionService.GetTransactionsByPayee(transactionPayee);
+
+            return await selectTransaction(transactions);
+        }
+
+
+
+        private static async Task<TransactionModel> selectTransaction(List<TransactionModel> transactions)
+        {
+            Console.Clear();
+            Console.WriteLine(transactions.ToStringTable(new[] { "Index", "Amount", "Date", "Payee", "Type" }, t => t.Index, t => t.Amount, t => t.Date, t => Regex.Match(t.Payee, ".{0,25}").Value, t => t.Type));
+
+
+            int selectedTransaction = 0;
+            do
+            {
+                Console.WriteLine("Please enter a transaction number or [C]ancel and press ENTER:");
+
+                try
+                {
+                    var consoleSelection = Console.ReadLine();
+
+                    if (consoleSelection.ToLower() == "c")
+                    {
+                        await LocalAttachmentMenu.Show();
+                    }
+
+                    selectedTransaction = Convert.ToInt32(consoleSelection);
+                }
+                catch (Exception)
+                {
+                    selectedTransaction = 0;
+                }
+            } while (!Enumerable.Range(1, transactions.Max(x => x.Index)).Contains(selectedTransaction));
+
+            return transactions.FirstOrDefault(x => x.Index == selectedTransaction);
         }
     }
 }
