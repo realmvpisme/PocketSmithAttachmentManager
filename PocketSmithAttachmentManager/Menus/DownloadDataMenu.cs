018127a0b43b4d7e2979c37a8a7cc6778cb33fa8
Nate Burdick <nate@thep4x.com>
2021-06-02 07:14:43 -0500

    In Progress.

---
+++ PocketSmithAttachmentManager/Menus/DownloadDataMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PocketSmithAttachmentManager.Menus
+{
+    public static class DownloadDataMenu
+    {
+        public static string MenuText = @"
+        *****Download Data*****
+
+    1. Download All Data
+    2. Download Transactions
+    3. Return To Main Menu";
+
+        public static async Task Show()
+        {
+            Console.Clear();
+            Console.WriteLine(MenuText);
+
+            int selectedOption = 0;
+            do
+            {
+                Console.WriteLine("Please select an option and press ENTER:");
+                try
+                {
+                    selectedOption = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    selectedOption = 0;
+                }
+            } while (!Enumerable.Range(1, 3).Contains(selectedOption));
+        }
+    }
+}
--- PocketSmithAttachmentManager/Menus/TransactionMenu.cs
+++ PocketSmithAttachmentManager/Menus/TransactionMenu.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using PocketSmithAttachmentManager.Models;
+using PocketSmithAttachmentManager.Services;
+using PocketSmithAttachmentManager.Services.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
-using PocketSmithAttachmentManager.Models;
-using PocketSmithAttachmentManager.Services;
-using PocketSmithAttachmentManager.Services.Extensions;
 
 namespace PocketSmithAttachmentManager.Menus
 {
@@ -50,24 +49,23 @@ namespace PocketSmithAttachmentManager.Menus
             switch (selectedOption)
             {
                 case 1:
-                {
-                    break;
-                }
+                    {
+                        Console.WriteLine("This function is not yet implemented.");
+                        break;
+                    }
 
                 case 2:
-                {
-
-                    break;
-                }
+                    {
+                        Console.WriteLine("This function is not yet implemented.");
+                        break;
+                    }
 
                 case 3:
-                {
-                    await MainMenu.Show();
-                    break;
-                }
+                    {
+                        await MainMenu.Show();
+                        break;
+                    }
             }
-
-
         }
 
         public static async Task<TransactionModel> GetTransactionByAmount()
@@ -137,7 +135,6 @@ namespace PocketSmithAttachmentManager.Menus
                     Console.Clear();
                     Console.WriteLine("Please enter an end date or [C]ancel and press ENTER:");
 
-
                     var selectedOption = Console.ReadLine();
                     if (selectedOption.ToLower() == "c")
                     {
@@ -150,7 +147,6 @@ namespace PocketSmithAttachmentManager.Menus
                 {
                     endDate = null;
                 }
-
             } while (endDate == null);
 
             var transactions = await _transactionService.GetTransactionsByDate(Convert.ToDateTime(startDate), endDate);
@@ -161,7 +157,6 @@ namespace PocketSmithAttachmentManager.Menus
             }
 
             return await selectTransaction(transactions);
-
         }
 
         public static async Task<TransactionModel> GetTransactionByPayee()
@@ -180,22 +175,18 @@ namespace PocketSmithAttachmentManager.Menus
             } while (string.IsNullOrEmpty(transactionPayee));
 
             var transactions = await _transactionService.GetTransactionsByPayee(transactionPayee);
-   foreach (var transaction in transactions)
+            foreach (var transaction in transactions)
             {
                 transaction.Attachments = await _attachmentService.GetByTransactionId(transaction.Id);
             }
-         
 
             return await selectTransaction(transactions);
         }
 
-
-
         private static async Task<TransactionModel> selectTransaction(List<TransactionModel> transactions)
         {
             Console.Clear();
-            Console.WriteLine(transactions.ToStringTable(new[] { "Index", "Amount", "Date", "Payee", "Type", "Account Name", "Attachments"}, t => t.Index, t => t.Amount, t => t.Date,t => Regex.Match(t.Payee, ".{0,25}").Value, t => t.Type, t => t.TransactionAccount.Name, t => t.Attachments.Count));
-
+            Console.WriteLine(transactions.ToStringTable(new[] { "Index", "Amount", "Date", "Payee", "Type", "Account Name", "Attachments" }, t => t.Index, t => t.Amount, t => t.Date, t => Regex.Match(t.Payee, ".{0,25}").Value, t => t.Type, t => t.TransactionAccount.Name, t => t.Attachments.Count));
 
             int selectedTransaction = 0;
             do
