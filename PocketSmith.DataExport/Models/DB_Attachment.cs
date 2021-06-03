cf11ac7d767f3fa97b77d13b12bfd2bb23a5019b
Nate Burdick <nate@thep4x.com>
2021-06-03 07:15:17 -0500

    In Progress.

---
+++ PocketSmith.DataExport/Models/DB_Attachment.cs
@@ -0,0 +1,18 @@
+﻿namespace PocketSmith.DataExport.Models
+{
+    public class DB_Attachment : ModelBase
+    {
+        public string Title { get; set; }
+        public string FileName { get; set; }
+        public string Type { get; set; }
+        public string ContentType { get; set; }
+        public string OriginalUrl { get; set; }
+        public long? VariantId { get; set; }
+        public DB_Variant Variants { get; set; }
+        public long? ContentTypeMetaId { get; set; }
+        public DB_ContentTypeMeta ContentTypeMeta { get; set; }
+        public long? TransactionId { get; set; }
+        public DB_Transaction Transaction { get; set; }
+
+    }
+}
---
+++ PocketSmith.DataExport/Models/ModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PocketSmith.DataExport.Models
+{
+    public class ModelBase
+    {
+        public long Id { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
