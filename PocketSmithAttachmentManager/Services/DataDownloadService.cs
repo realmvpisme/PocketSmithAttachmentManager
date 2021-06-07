bae7dfd0e0a3093305b89e2dfa883aa089ab1418
Nate Burdick <nate@thep4x.com>
2021-06-07 07:14:32 -0500

    In Progress.

--- PocketSmithAttachmentManager/Services/DataDownloadService.cs
+++ PocketSmithAttachmentManager/Services/DataDownloadService.cs
@@ -13,6 +13,7 @@ namespace PocketSmithAttachmentManager.Services
         private readonly HttpClient _httpClient;
         private readonly Type _parentType;
         private PocketSmithDbContext _context;
+        private TransactionService _transactionService;
 
         public DataDownloadService(Type parentType)
         {
@@ -25,22 +26,62 @@ namespace PocketSmithAttachmentManager.Services
                 .Add("Accept", "application/json");
 
             _parentType = parentType;
+
+            _transactionService = new TransactionService();
         }
 
         public async Task DownloadAllData()
         {
+            if (_context == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("A database file must be loaded before downloading data.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
 
+            Console.WriteLine("Downloading transactions...");
         }
 
         public async Task<bool> LoadDatabase(string filePath)
-        { 
+        {
             var contextOptionsBuilder = new DbContextOptionsBuilder();
             contextOptionsBuilder.UseSqlite($"Data Source={filePath}");
             await using var _context = new PocketSmithDbContext(contextOptionsBuilder.Options);
 
-            await _context.Database.MigrateAsync();
+            bool fileExists = File.Exists(filePath);
+
+            Console.Clear();
+            if (fileExists)
+            {
+                Console.WriteLine("Loading database file...");
+            }
+            else
+            {
+                Console.WriteLine("Database file does not exist. Creating new database file...");
+            }
+
+            try
+            {
+                await _context.Database.MigrateAsync();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Database file failed to load.");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Database file loaded successfully.");
+            Console.ForegroundColor = ConsoleColor.White;
 
             return true;
+        }
+
+        private async Task downloadTransactions()
+        {
+
         }
 
     }
