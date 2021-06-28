db9298fd1818119bcf3c7c0bf191e303901e56e2
Nate Burdick <nate@thep4x.com>
2021-06-28 07:15:30 -0500

    In Progress.

---
+++ PocketSmith.DataExportServices/Budget/BudgetEventDataService.cs
@@ -0,0 +1,13 @@
+﻿using PocketSmith.DataExport.Models;
+using PocketSmith.DataExportServices.JsonModels;
+
+namespace PocketSmith.DataExportServices.Budget
+{
+    public class BudgetEventDataService : DataServiceBase<BudgetEventModel, DB_BudgetEvent>
+    {
+        public BudgetEventDataService(string databaseFilePath) : base(databaseFilePath)
+        {
+
+        }
+    }
+}
--- PocketSmith.DataExportServices/MapperConfigurationGenerator.cs
+++ PocketSmith.DataExportServices/MapperConfigurationGenerator.cs
@@ -1,6 +1,6 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using AutoMapper;
+﻿using AutoMapper;
 using PocketSmith.DataExportServices.Accounts.Mappers;
+using PocketSmith.DataExportServices.Budget.Mappers;
 using PocketSmith.DataExportServices.Categories.Mappers;
 using PocketSmith.DataExportServices.Institutions.Mappers;
 using PocketSmith.DataExportServices.Transactions.Mappers;
@@ -17,6 +17,8 @@ namespace PocketSmith.DataExportServices
                cfg.AddProfile(new AccountMapperProfile());
                cfg.AddProfile(new CategoryMapperProfile());
                cfg.AddProfile(new InstitutionMapperProfile());
+               cfg.AddProfile(new ScenarioMapperProfile());
+               cfg.AddProfile(new BudgetEventMapperProfile());
             });
         }
     }
