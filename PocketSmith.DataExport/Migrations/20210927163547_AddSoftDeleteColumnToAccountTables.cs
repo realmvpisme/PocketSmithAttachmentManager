530dad10738c89818c6fc590a8760a5d9812f114
Nate Burdick <nate@thep4x.com>
2021-09-27 11:47:58 -0500

    #10 Added soft delete for accounts.

---
+++ PocketSmith.DataExport/Migrations/20210927163547_AddSoftDeleteColumnToAccountTables.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PocketSmith.DataExport.Migrations
+{
+    public partial class AddSoftDeleteColumnToAccountTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "TransactionAccounts",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Accounts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "TransactionAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Accounts");
+        }
+    }
+}
